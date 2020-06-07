2684fc547893e4fd8a4be16bcdf8e5c29d3f51e0
netkas <netkas@intellivoid.info>
2020-06-07 00:36:22 -0400

    Updated install.sh

--- install.sh
+++ install.sh
@@ -14,26 +14,25 @@ COFFEEHOUSEMOD_STOPWORDS_VERSION="1.0.0"
 HYPER_INTERNAL_SERVICE_VERSION="1.0.0"
 COFFEEHOUSE_SPAMDETECTION_VERSION="1.0.0"
 
-echo " #### Downloading all git repos"
-git clone https://${GIT_API_KEY}@github.com/${GIT_ORGANIZATION_NAME}/CoffeeHouse-DLTC.git
-git clone https://${GIT_API_KEY}@github.com/${GIT_ORGANIZATION_NAME}/CoffeeHouse-NLPFR.git
-git clone https://${GIT_API_KEY}@github.com/${GIT_ORGANIZATION_NAME}/CoffeeHouse-SpamDetection.git
-git clone https://${GIT_API_KEY}@github.com/${GIT_ORGANIZATION_NAME}/CoffeeHouseMod-APT.git
-git clone https://${GIT_API_KEY}@github.com/${GIT_ORGANIZATION_NAME}/CoffeeHouseMod-Tokenizer.git
-git clone https://${GIT_API_KEY}@github.com/${GIT_ORGANIZATION_NAME}/CoffeeHouseMod-StopWords.git
-git clone https://${GIT_API_KEY}@github.com/${GIT_ORGANIZATION_NAME}/Hyper-Internal-Service.git
 
 echo " #### Installing NLPFR"
+git clone https://${GIT_API_KEY}@github.com/${GIT_ORGANIZATION_NAME}/CoffeeHouse-NLPFR.git
 cd CoffeeHouse-NLPFR && git checkout ${COFFEEHOUSE_NLPFR_VERSION} && python3 setup.py install && cd ..
+git clone https://${GIT_API_KEY}@github.com/${GIT_ORGANIZATION_NAME}/CoffeeHouseMod-APT.git
 cd CoffeeHouseMod-APT && git checkout ${COFFEEHOUSEMOD_APT_VERSION} && python3 setup.py install && cd ..
+git clone https://${GIT_API_KEY}@github.com/${GIT_ORGANIZATION_NAME}/CoffeeHouseMod-Tokenizer.git
 cd CoffeeHouseMod-Tokenizer && git checkout ${COFFEEHOUSEMOD_TOKENIZER_VERSION} && python3 setup.py install && cd ..
+git clone https://${GIT_API_KEY}@github.com/${GIT_ORGANIZATION_NAME}/CoffeeHouseMod-StopWords.git
 cd CoffeeHouseMod-StopWords && git checkout ${COFFEEHOUSEMOD_STOPWORDS_VERSION} && python3 setup.py install && cd ..
 
 echo " #### Installing DLTC"
+git clone https://${GIT_API_KEY}@github.com/${GIT_ORGANIZATION_NAME}/CoffeeHouse-DLTC.git
 cd CoffeeHouse-DLTC && git checkout ${COFFEEHOUSE_DLTC_VERSION} && python3 setup.py install && cd ..
 
 echo " #### Installing Services"
+git clone https://${GIT_API_KEY}@github.com/${GIT_ORGANIZATION_NAME}/Hyper-Internal-Service.git
 cd Hyper-Internal-Service && git checkout ${HYPER_INTERNAL_SERVICE_VERSION} && python3 -m pip install -Ur dev_requirements.txt && python3 setup.py install && cd ..
+git clone https://${GIT_API_KEY}@github.com/${GIT_ORGANIZATION_NAME}/CoffeeHouse-SpamDetection.git
 cd CoffeeHouse-SpamDetection && git checkout ${COFFEEHOUSE_SPAMDETECTION_VERSION} && python3 setup.py install && cd ..
 
 echo " #### Cleaning up"
